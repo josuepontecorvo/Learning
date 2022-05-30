dc24076ff261b2a4fbf1a961174075c8d0ed53c1
josuepontecorvo <josuepontecorvo@gmail.com>
2022-05-29 23:57:34 -0300

    crear blockchain

--- app.js
+++ app.js
@@ -1,13 +1,43 @@
-function contar (value) {
-    let contador = value;
-    return function () {
-       console.log( ++contador );
+const SHA256 = require("crypto-js/sha256");
+class Block {
+    constructor(index,timestamp,data,previoushash = "") {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previoushash = previoushash;
+        this.hash = this.calculateHash();
+    }
+    calculateHash() {
+        return SHA256(JSON.stringify(this.data)+this.previoushash+this.index+this.timestamp).toString();
     }
 }
 
-let prueba1 = contar(5);
-let prueba2 = contar(10);
+class Blockchain {
+    constructor(){
+        this.chain = [this.createGenesisBlock()];
+    }
+    createGenesisBlock (){
+        return new Block (0,"01/01/2017","Genesis Block","0") ;
+    }
+    getLatestBlock() {
+        return this.chain[this.chain.length-1];
+    }
+    addBlock(newBlock) {
+        newBlock.previoushash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+    
+}
 
-prueba1();
-prueba2();
+let chain1 = new Blockchain();
+let trans1 = new Block(1,"10/05/2022","coin1");
+chain1.addBlock(trans1);
+let trans2 = new Block(2,"11/05/2022","coin2");
+chain1.addBlock(trans2);
+let trans3 = new Block(3,"12/05/2022","coin3");
+chain1.addBlock(trans3);
+
+console.log(chain1.chain);
+
 
